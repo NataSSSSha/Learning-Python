d676265fbaa2c75779e1b01b3c88e3c23f050d9c
NataliaVA <vanatalia1@gmail.com>
2022-10-11 23:41:41 +0400

    добавлено появление 3-х шариков в начале игры

--- NewProject/game.py
+++ NewProject/game.py
@@ -1,10 +1,13 @@
 import tkinter
 import game_model
+import random
 
 
 class Game:
     def __init__(self):
         self.model = game_model.GameModel()
+
+    matrix = [[0 for x in range(6)] for y in range(6)]
 
     def show_display(self):
         tk = tkinter.Tk()
@@ -21,10 +24,18 @@ class Game:
                                         i * self.model.square_size + self.model.square_size,
                                         j * self.model.square_size + self.model.square_size,
                                         fill='gray')
-                canvas.create_oval(i * self.model.square_size + self.model.oval_size,
-                                   j * self.model.square_size + self.model.oval_size,
-                                   i * self.model.square_size + self.model.square_size - self.model.oval_size,
-                                   j * self.model.square_size + self.model.square_size - self.model.oval_size,
-                                   fill=self.model.color_array[i])
+
+        count = 0
+        while count < 3:
+            new_ball_coord_first = random.randint(0, self.model.grid_size - 1)
+            new_ball_coord_second = random.randint(0, self.model.grid_size - 1)
+            if self.matrix[new_ball_coord_first][new_ball_coord_second] == 0:
+                canvas.create_oval(new_ball_coord_first * self.model.square_size + self.model.oval_size,
+                                   new_ball_coord_second * self.model.square_size + self.model.oval_size,
+                                   new_ball_coord_first * self.model.square_size + self.model.square_size - self.model.oval_size,
+                                   new_ball_coord_second * self.model.square_size + self.model.square_size - self.model.oval_size,
+                                   fill=self.model.color_array[new_ball_coord_first])
+                self.matrix[new_ball_coord_first][new_ball_coord_second] = 1
+                count += 1
 
         tk.mainloop()
--- NewProject/main.py
+++ NewProject/main.py
@@ -3,7 +3,7 @@ import game
 
 new_game = game.Game()
 new_game.show_display()
-
+#new_game.add_balls()
 
 
 
